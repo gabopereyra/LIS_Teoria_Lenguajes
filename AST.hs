7ef899be54c04a8f9d04a39fa74b0025b1269caa
Gabriel Pereyra <gabrielp1689@gmail.com>
2025-02-07 19:37:50 -0300

    update AST file

--- AST.hs
+++ AST.hs
@@ -3,33 +3,42 @@ module AST where
 -- Identificadores de Variable
 type Variable = String
 
--- Expresiones Aritmeticas
-data IntExp = Const Integer
-            | Var Variable
-            | UMinus IntExp
-            | Plus IntExp IntExp
-            | Minus IntExp IntExp
-            | Times IntExp IntExp
-            | Div IntExp IntExp
-            | Question BoolExp IntExp IntExp -- a > 0 ? 1 : 2;
- deriving (Show,Eq)
+-- Expresiones Aritméticas (Enteras y Flotantes)
+data Exp = Const Integer
+         | FloatConst Double
+         | Var Variable
+         | UMinus Exp
+         | Plus Exp Exp
+         | Minus Exp Exp
+         | Times Exp Exp
+         | Div Exp Exp
+         | Question BoolExp Exp Exp
+         | Sin Exp
+         | Cos Exp
+         | Tan Exp
+         | Ceil Exp
+         | Floor Exp
+         | Pi
+    deriving (Show, Eq)
 
 -- Expresiones Booleanas
 data BoolExp = BTrue
              | BFalse
-             | Eq IntExp IntExp
-             | Lt IntExp IntExp
-             | Gt IntExp IntExp
+             | Eq Exp Exp
+             | Lt Exp Exp
+             | Gt Exp Exp
              | And BoolExp BoolExp
              | Or BoolExp BoolExp
              | Not BoolExp
- deriving (Show,Eq)
+    deriving (Show, Eq)
 
--- Comandos (sentencias)
--- Observar que solo se permiten variables de un tipo (entero)
+-- Comandos (Sentencias)
 data Comm = Skip
-          | Let Variable IntExp
+          | Let Variable Exp
           | Seq Comm Comm
           | Cond BoolExp Comm Comm
           | Repeat Comm BoolExp
- deriving (Show,Eq)
+          | Input Variable
+          | Print (Maybe String) Exp
+          | Tic String
+    deriving (Show, Eq)
